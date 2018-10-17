515c2a74761eff5be7559788afabfadf2a933216
Karl Velazquez <karlvelaz350@gmail.com>
2018-10-17 16:55:19 -0700

    added basic chassis motor movement functions for each side

--- 12-1-18_competition_code/include/chassis.h
+++ 12-1-18_competition_code/include/chassis.h
@@ -8,4 +8,19 @@ extern Motor rightChassis1;
 extern Motor rightChassis2;
 extern Motor rightChassis3;
 
+void moveLeftChassis(int speed);
+void moveRightChassis(int speed);
+
+void move_absoluteLeftChassis(int distance);
+void move_absoluteRightChassis(int distance);
+
+void move_relativeLeftChassis(int distance);
+void move_relativeRightChassis(int distance);
+
+void move_velocityLeftChassis(int rpm);
+void move_velocityRightChassis(int rpm);
+
+void move_voltageLeftChassis(int voltage);
+void move_voltageRightChassis(int voltage);
+
 #endif
--- 12-1-18_competition_code/src/chassis.cpp
+++ 12-1-18_competition_code/src/chassis.cpp
@@ -6,3 +6,83 @@ Motor leftChassis3(3, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_DEGREES); //Port 3, 200
 Motor rightChassis1(4, MOTOR_GEARSET_18, 1, MOTOR_ENCODER_DEGREES); //Port 4, 200 RPM, reversed, degrees for encoder units
 Motor rightChassis2(5, MOTOR_GEARSET_18, 1, MOTOR_ENCODER_DEGREES); //Port 5, 200 RPM, reversed, degrees for encoder units
 Motor rightChassis3(6, MOTOR_GEARSET_18, 1, MOTOR_ENCODER_DEGREES); //Port 6, 200 RPM, reversed, degrees for encoder units
+
+void moveLeftChassis(int speed) {
+
+    leftChassis1.move(speed);
+    leftChassis2.move(speed);
+    leftChassis3.move(speed);
+
+}
+
+void moveRightChassis(int speed) {
+
+    rightChassis1.move(speed);
+    rightChassis2.move(speed);
+    rightChassis3.move(speed);
+
+}
+
+void move_absoluteLeftChassis(int distance) {
+
+    leftChassis1.move_absolute(distance);
+    leftChassis2.move_absolute(distance);
+    leftChassis3.move_absolute(distance);
+
+}
+
+void move_absoluteRightChassis(int distance) {
+
+    rightChassis1.move_absolute(distance);
+    rightChassis2.move_absolute(distance);
+    rightChassis3.move_absolute(distance);
+
+}
+
+void move_relativeLeftChassis(int distance) {
+
+    leftChassis1.move_relative(distance);
+    leftChassis2.move_relative(distance);
+    leftChassis3.move_relative(distance);
+
+}
+
+void move_relativeRightChassis(int distance) {
+
+    rightChassis1.move_relative(distance);
+    rightChassis2.move_relative(distance);
+    rightChassis3.move_relative(distance);
+
+}
+
+void move_velocityLeftChassis(int rpm) {
+
+    leftChassis1.move_velocity(rpm);
+    leftChassis2.move_velocity(rpm);
+    leftChassis3.move_velocity(rpm);
+
+}
+
+void move_velocityRightChassis(int rpm) {
+
+    rightChassis1.move_velocity(rpm);
+    rightChassis2.move_velocity(rpm);
+    rightChassis3.move_velocity(rpm);
+
+}
+
+void move_voltageLeftChassis(int voltage) {
+
+    leftChassis1.move_voltage(voltage);
+    leftChassis2.move_voltage(voltage);
+    leftChassis3.move_voltage(voltage);
+
+}
+
+void move_voltageRightChassis(int voltage) {
+
+    rightChassis1.move_voltage(voltage);
+    rightChassis2.move_voltage(voltage);
+    rightChassis3.move_voltage(voltage);
+
+}
