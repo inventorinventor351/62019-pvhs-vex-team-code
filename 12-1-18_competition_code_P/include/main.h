fc7079296800f5329aaf1ba236e38ea04d046d17
Karl Velazquez <karlvelaz350@gmail.com>
2018-10-21 10:40:06 -0700

    adding pragma header and source files to organize motor initialization

--- 12-1-18_competition_code_P/include/main.h
+++ 12-1-18_competition_code_P/include/main.h
@@ -14,6 +14,7 @@ using namespace pros; //Instead of typing pros::Motor() all the time, if you use
 
 //You should include more files right under here
 //ex: #include "file.h"
+#include "pragma.h"
 #include "chassis.h"
 
 //DO NOT TOUCH BELOW STUFF, THIS MAKES THE CODE RUN
