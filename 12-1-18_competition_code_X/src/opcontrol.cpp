937d3f78895967d73dfce50026d873ac6210c116
Chris Salas <chrissalas@Chriss-MacBook-Pro.local>
2018-10-21 14:42:27 -0700

    added pragma for x team

--- 12-1-18_competition_code_X/src/opcontrol.cpp
+++ 12-1-18_competition_code_X/src/opcontrol.cpp
@@ -3,10 +3,30 @@
 //Runs during the 1 minute and 45 second period of driver control or immediately after initialize() if no competition control is connected
 void opcontrol() {
 	
-	while(true) {
+	Controller master(CONTROLLER_MASTER);
 
 
-		
+	while(true) 
+	{
+		driveRight((master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)));
+		driveLeft((master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)));
+
+		if(master.get.digital(E_CONTROLLER_DIGITAL_R1))
+			{
+				intake.move(120);
+			}
+		if(master.get.digital(E_CONTROLLER_DIGITAL_L1))
+			{
+				intake.move(-120);
+			}
+
+		if(master.get.digital(E_CONTROLLER_DIGITAL_L2) && (E_CONTROLLER_DIGITAL_R2))
+			{ 
+				while()
+				shooter.move(110);
+
+			}
+		delay(1);
 	}
 
 }
