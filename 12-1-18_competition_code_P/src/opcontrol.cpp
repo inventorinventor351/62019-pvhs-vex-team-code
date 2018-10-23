85988bc017b769a572f71eb8335fb6de2539aaef
Karl Velazquez <karlvelaz350@gmail.com>
2018-10-23 14:44:55 -0700

    removed driver code except left chassis code

--- 12-1-18_competition_code_P/src/opcontrol.cpp
+++ 12-1-18_competition_code_P/src/opcontrol.cpp
@@ -11,41 +11,7 @@ void opcontrol() {
 		move_velocityLeftChassis((master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y)/127) * 200);
 		move_velocityRightChassis((master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)/127) * 200);
 
-		if(master.get_digital(E_CONTROLLER_DIGITAL_L1)) {
-
-			intake.move(127);
-
-		}
-
-		else if(master.get_digital(E_CONTROLLER_DIGITAL_R1)) {
-
-			intake.move(-127);
-			
-		}
-
-		else {
-
-			intake.move(0);
-
-		}
-
-		if(partner.get_digital(E_CONTROLLER_DIGITAL_L1) && partner.get_digital(E_CONTROLLER_DIGITAL_R1)) {
-
-			catapult.move(127);
-
-		}
-
-		else if(partner.get_digital(E_CONTROLLER_DIGITAL_L2) && partner.get_digital(E_CONTROLLER_DIGITAL_R2)) {
-
-			catapult.move(-127);
-			
-		}
-
-		else {
-
-			catapult.move(0);
-
-		}
+		
 
 		delay(1);
 		
