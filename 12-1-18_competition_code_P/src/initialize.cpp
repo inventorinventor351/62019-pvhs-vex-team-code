248bdf90867d35298813c0fdf55c92c28fe1ea07
Karl Velazquez <karlvelaz350@gmail.com>
2018-10-27 13:16:56 -0700

    stuff

--- 12-1-18_competition_code_P/src/initialize.cpp
+++ 12-1-18_competition_code_P/src/initialize.cpp
@@ -6,10 +6,8 @@ void initialize() {
     motor_pid_s_t chassisPID = Motor::convert_pid(0, 1.0, 0.001, 0.1); //PID constants for chassis
     leftChassis1.set_pos_pid(chassisPID);
     leftChassis2.set_pos_pid(chassisPID);
-    leftChassis3.set_pos_pid(chassisPID);
     rightChassis1.set_pos_pid(chassisPID);
     rightChassis2.set_pos_pid(chassisPID);
-    rightChassis3.set_pos_pid(chassisPID);
 
     catapultEye.clear_led();
 
--- 12-1-18_competition_code_P/src/opcontrol.cpp
+++ 12-1-18_competition_code_P/src/opcontrol.cpp
@@ -13,23 +13,24 @@ void opcontrol() {
 		move_velocityLeftChassis((master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y)/127) * 200); 
 		move_velocityRightChassis((master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)/127) * 200);
 
-		if((master.get_digital(E_CONTROLLER_DIGITAL_L1)) && (master.get_digital(E_CONRTOLLER_DIGITAL_R1))) {
+		if(master.get_digital(E_CONTROLLER_DIGITAL_L1) && master.get_digital(E_CONRTOLLER_DIGITAL_R1)) {
 
 			catapult.move_relative(1, 200);
 
 		}
 
-		if((master.get_digital(E_CONTROLLER_R2))){
+		if(master.get_digital(E_CONTROLLER_R2)) {
 
 			intake.move_velocity(200);
-		
+	
 		}
 
-		if((master.get_digital(E_CONTROLLER_L2))){
+		if((master.get_digital(E_CONTROLLER_L2))) {
 
 			intake.move_velocity(-200);
 
 		}
+
 		/*move_absoluteLeftChassis(setPoint, 200);
 		move_absoluteRightChassis(setPoint, 200);
 
