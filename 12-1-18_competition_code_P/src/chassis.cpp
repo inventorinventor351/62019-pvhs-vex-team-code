6792a2a1f5644972fb63d7e66bd536ca2a727d9e
CameronG <cam.greene@gmail.com>
2018-10-27 12:33:40 -0700

    removed third motor from chassis

--- 12-1-18_competition_code_P/src/chassis.cpp
+++ 12-1-18_competition_code_P/src/chassis.cpp
@@ -4,7 +4,7 @@ void moveLeftChassis(int speed) {
 
     leftChassis1.move(speed);
     leftChassis2.move(speed);
-    leftChassis3.move(speed);
+  
 
 }
 
@@ -12,7 +12,7 @@ void moveRightChassis(int speed) {
 
     rightChassis1.move(speed);
     rightChassis2.move(speed);
-    rightChassis3.move(speed);
+
 
 }
 
@@ -20,7 +20,7 @@ void move_absoluteLeftChassis(int distance, int maxSpeed) {
 
     leftChassis1.move_absolute(distance, maxSpeed);
     leftChassis2.move_absolute(distance, maxSpeed);
-    leftChassis3.move_absolute(distance, maxSpeed);
+
 
 }
 
@@ -28,7 +28,7 @@ void move_absoluteRightChassis(int distance, int maxSpeed) {
 
     rightChassis1.move_absolute(distance, maxSpeed);
     rightChassis2.move_absolute(distance, maxSpeed);
-    rightChassis3.move_absolute(distance, maxSpeed);
+  
 
 }
 
@@ -36,7 +36,7 @@ void move_relativeLeftChassis(int distance, int maxSpeed) {
 
     leftChassis1.move_relative(distance, maxSpeed);
     leftChassis2.move_relative(distance, maxSpeed);
-    leftChassis3.move_relative(distance, maxSpeed);
+   
 
 }
 
@@ -44,7 +44,7 @@ void move_relativeRightChassis(int distance, int maxSpeed) {
 
     rightChassis1.move_relative(distance, maxSpeed);
     rightChassis2.move_relative(distance, maxSpeed);
-    rightChassis3.move_relative(distance, maxSpeed);
+  
 
 }
 
@@ -52,7 +52,7 @@ void move_velocityLeftChassis(int rpm) {
 
     leftChassis1.move_velocity(rpm);
     leftChassis2.move_velocity(rpm);
-    leftChassis3.move_velocity(rpm);
+ 
 
 }
 
@@ -60,7 +60,7 @@ void move_velocityRightChassis(int rpm) {
 
     rightChassis1.move_velocity(rpm);
     rightChassis2.move_velocity(rpm);
-    rightChassis3.move_velocity(rpm);
+
 
 }
 
@@ -68,7 +68,7 @@ void move_voltageLeftChassis(int voltage) {
 
     leftChassis1.move_voltage(voltage);
     leftChassis2.move_voltage(voltage);
-    leftChassis3.move_voltage(voltage);
+
 
 }
 
@@ -76,19 +76,19 @@ void move_voltageRightChassis(int voltage) {
 
     rightChassis1.move_voltage(voltage);
     rightChassis2.move_voltage(voltage);
-    rightChassis3.move_voltage(voltage);
+
 
 }
 
 float getLeftChassisPosition() {
 
-    return (leftChassis1.get_position() + leftChassis2.get_position() + leftChassis3.get_position())/3;
+    return (leftChassis1.get_position() + leftChassis2.get_position())/2;
 
 }
 
 float getRightChassisPosition() {
 
-    return (rightChassis1.get_position() + rightChassis2.get_position() + rightChassis3.get_position())/3;
+    return (rightChassis1.get_position() + rightChassis2.get_position())/2;
     
 }
 
