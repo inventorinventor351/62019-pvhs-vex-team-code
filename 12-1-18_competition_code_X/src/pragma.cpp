0ca6456d7b25e066bf53475b70eff83f715e6fe6
AaronG <aaronguo213@gmail.com>
2018-10-27 13:32:29 -0700

    added seudo code to the pragma

--- 12-1-18_competition_code_X/src/pragma.cpp
+++ 12-1-18_competition_code_X/src/pragma.cpp
@@ -2,20 +2,21 @@
 
 Motor leftChassis1(1, motor_gearset_18, 0, e_motor_encoder_rotation); //Port 1, 200 RPM gearset, not reversed, units of rotations for encoder
 Motor leftChassis2(2, motor_gearset_18, 0, e_motor_encoder_rotation); //Port 2, 200 RPM gearset, not reversed, units of rotations for encoder
-Motor rightChassis1(4, motor_gearset_18, 1, e_motor_encoder_rotation); //Port 4, 200 RPM gearset, reversed, units of rotations for encoder
-Motor rightChassis2(5, motor_gearset_18, 1, e_motor_encoder_rotation); //Port 5, 200 RPM gearset, reversed, units of rotations for encoder
-Motor intake(7, motor_gearset_18, 0, e_motor_encoder_rotation); //Port 7, 200 RPM gearset, not reversed, units of rotations for encoder
-Motor shooter(8, motor_gearset_18, 0, e_motor_encoder_rotation); //Port 8, 200 RPM gearset, not reversed, units of rotations for encoder
+Motor rightChassis1(3, motor_gearset_18, 1, e_motor_encoder_rotation); //Port 3, 200 RPM gearset, reversed, units of rotations for encoder
+Motor rightChassis2(4, motor_gearset_18, 1, e_motor_encoder_rotation); //Port 4, 200 RPM gearset, reversed, units of rotations for encoder
+Motor intake(5, motor_gearset_18, 0, e_motor_encoder_rotation); //Port 5, 200 RPM gearset, not reversed, units of rotations for encoder
+Motor shooter(6, motor_gearset_18, 0, e_motor_encoder_rotation); //Port 6, 200 RPM gearset, not reversed, units of rotations for encoder
 
-Vision shooterEye();
+Vision shooterEye(7); //Port 7, vision sensor for shooter
 
-ADIAnalogIn potentiometer(1); //Port 1, potentiometer for shooter
-ADIAnalogIn ballSensor(3); //Port 3, senses when the ball passes into the shooter
 
-ADIDigitalOut intakeLift();
-ADIDigitalOut intakeLift1();
+ADIDigitalOut intakeLift(1); //Port 1, solonoid for raising intake
+ADIDigitalOut intakeLift1(2); //Port 2, solonoid for raising intake
 
-ADIDigitalOut leftChassisTrans();
-ADIDigitalOut rightChassisTrans();
+ADIDigitalOut leftChassisTrans(3); //Port 3, solonoid for changing strenght and speed on left chassis
+ADIDigitalOut rightChassisTrans(4); //Port 4, solonoid for changing strength and speed on right chassis
 
-ADIDigital tipper();
+ADIDigital tipper(5); //Port 5, solonoid for cap tipping
+
+ADIAnalogIn potentiometer(6); //Port 6, potentiometer for shooter
+ADIAnalogIn ballSensor(7); //Port 7, light sensor for the ball
