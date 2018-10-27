6bb8ebaee9354339a18bf93c416cffadc40de571
CameronG <cam.greene@gmail.com>
2018-10-27 13:10:12 -0700

    opcontrol added instake and catapult

--- 12-1-18_competition_code_P/src/opcontrol.cpp
+++ 12-1-18_competition_code_P/src/opcontrol.cpp
@@ -10,14 +10,31 @@ void opcontrol() {
 	
 	while(true) {
 
-		//move_velocityLeftChassis((master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y)/127) * 200);
-		//move_velocityRightChassis((master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)/127) * 200);
+		move_velocityLeftChassis((master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y)/127) * 200); 
+		move_velocityRightChassis((master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)/127) * 200);
 
-		move_absoluteLeftChassis(setPoint, 200);
+		if((master.get_digital(E_CONTROLLER_DIGITAL_L1)) && (master.get_digital(E_CONRTOLLER_DIGITAL_R1))) {
+
+			catapult.move_relative(1, 200);
+
+		}
+
+		if((master.get_digital(E_CONTROLLER_R2))){
+
+			intake.move_velocity(200);
+		
+		}
+
+		if((master.get_digital(E_CONTROLLER_L2))){
+
+			intake.move_velocity(-200);
+
+		}
+		/*move_absoluteLeftChassis(setPoint, 200);
 		move_absoluteRightChassis(setPoint, 200);
 
 		std::cout << "Left Error: " << (setPoint - getLeftChassisPosition());
-		std::cout << "Right Error: " << (setPoint - getRightChassisPosition());
+		std::cout << "Right Error: " << (setPoint - getRightChassisPosition());*/
 
 		delay(1);
 		
