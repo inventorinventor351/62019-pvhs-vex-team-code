cda71abde717d9fa6c0f18aa074d7de2a4e1f04c
Karl Velazquez <karlvelaz350@gmail.com>
2018-10-27 12:19:59 -0700

    fixing pragma

--- 12-1-18_competition_code_P/include/pragma.h
+++ 12-1-18_competition_code_P/include/pragma.h
@@ -3,15 +3,20 @@
 
 extern Motor leftChassis1;
 extern Motor leftChassis2;
-extern Motor leftChassis3;
 extern Motor rightChassis1;
 extern Motor rightChassis2;
-extern Motor rightChassis3;
 extern Motor intake;
 extern Motor catapult;
 
-extern ADIAnalogIn catapultPotent;
-extern ADIAnalogIn ballLoaderSensor; 
 extern Vision catapultEye;
+
+extern ADIDigitalOut intakePiston1;
+extern ADIDigitalOut intakePiston2;
+extern ADIDigitalOut leftChassisTransmissionPiston;
+extern ADIDigitalOut rightChassisTransmissionPiston;
+extern ADIDigitalOut capDescorerPiston;
+
+extern ADIAnalogIn catapultPotent;
+extern ADIAnalogIn ballLoaderLightSensor;
 
 #endif
