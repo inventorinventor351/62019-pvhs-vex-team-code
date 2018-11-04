f31296cb78b90b35cf6de28b48439f1f604dab74
Karl Velazquez <karlvelaz350@gmail.com>
2018-11-04 12:45:41 -0800

    created an autonomous selector

--- 12-1-18_competition_code_P/src/autonomous.cpp
+++ 12-1-18_competition_code_P/src/autonomous.cpp
@@ -3,6 +3,40 @@
 //Runs during the 15 second autonomous period
 void autonomous() {
 
-    red_flagSide_3flags_1cap_auton();
+    if(autonCount == 0) {
+
+        red_flagSide_3flags_1cap_auton();
+
+    }
+
+    else if(autonCount == 1) {
+
+
+
+    }
+
+    else if(autonCount == 2) {
+
+
+
+    }
+
+    else if(autonCount == 3) {
+
+
+
+    }
+
+    else if(autonCount == 4) {
+
+
+
+    }
+
+    else if(autonCount == 5) {
+
+
+
+    }
     
 }
