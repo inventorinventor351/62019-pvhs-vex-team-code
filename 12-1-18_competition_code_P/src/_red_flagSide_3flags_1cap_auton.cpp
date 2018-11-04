3e3b4e935d08423b200526875af33bb480c1185a
CameronG <cam.greene@gmail.com>
2018-11-04 12:39:41 -0800

    lmao

--- 12-1-18_competition_code_P/src/_red_flagSide_3flags_1cap_auton.cpp
+++ 12-1-18_competition_code_P/src/_red_flagSide_3flags_1cap_auton.cpp
@@ -2,42 +2,8 @@
 
 void red_flagSide_3flags_1cap_auton() {
 
-<<<<<<< HEAD
-    resetChassisEncoderValue();
-
-    move_absoluteLeftChassis(3.38, 200);
-    move_absoluteRightChassis(3.38, 200);
-
-    while(getLeftChassisPosition() < 2.75 && getRightChassisPosition() < 2.75) {
-    
-        intake.move_velocity(200);
-        delay(1);
-
-    }
-    
-    delay(250);
-    intake.move_velocity(0);
-
-    while(getLeftChassisPosition() < 3.38 && getRightChassisPosition() < 3.38) {
-    
-        delay(1);
-
-    }
-
-    move_absoluteLeftChassis(0, 200);
-    move_absoluteRightChassis(0, 200);
-
-    while(getLeftChassisPosition() > 0 && getRightChassisPosition() > 0) {
-    
-        delay(1);
-
-    }
-
-    pivotChassis(90, 200, 1000);
-
-    resetChassisEncoderValue();
-=======
     autonShoot();//shoot ball
+    pivotChassis(-90, 200, 1000)//face cap
     move_absoluteRightChassis(IntoRot(42),200); // move to cap
     move_absoluteLeftChassis(IntoRot(42),200); // move to cap
 
@@ -67,8 +33,9 @@ void red_flagSide_3flags_1cap_auton() {
 
     
 
+    
 
 
->>>>>>> 15c946952fb3582b77f20b754691a03b1f587766
+
 
 }
