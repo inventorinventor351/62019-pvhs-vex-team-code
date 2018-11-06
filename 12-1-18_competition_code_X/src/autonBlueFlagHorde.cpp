58dc5eb9af22506a2ee1f8ba47357419ba74d338
AaronG <aaronguo213@gmail.com>
2018-11-06 15:43:35 -0800

    finished flag side cap horde auton with the seudocoded op synergy

--- 12-1-18_competition_code_X/src/autonBlueFlagHorde.cpp
+++ 12-1-18_competition_code_X/src/autonBlueFlagHorde.cpp
@@ -3,7 +3,7 @@
 void autonRedFlagHorde(){
 
     autonAimFlag();
-    pvitChassis(90, 200, 1);
+    pvitChassis(-90, 200, 1);
 
     driveAbsLeft(45, 200);
     driveAbsRight(45, 200);
@@ -19,7 +19,7 @@ void autonRedFlagHorde(){
     intake.move(0);
     intakeLift.set_value(1);
 
-    pvitChassis(90, 200, 1); //Check to see if this flips the lower flag in the middle?
+    pvitChassis(-90, 200, 1); //Check to see if this flips the lower flag in the middle?
     driveRelativeLeft(30, 200);
     driveRelativeRight(30, 200);
     delay(300);
@@ -29,7 +29,7 @@ void autonRedFlagHorde(){
 
     driveRelativeLeft(-6, 200);
     driveRelativeRight(-6, 200);
-    pvitChassis(90, 200, 1);
+    pvitChassis(-90, 200, 1);
     driveRelativeLeft(5, 100);
     driveRelativeRight(5, 100);
     intake.move(-127);
@@ -43,7 +43,7 @@ void autonRedFlagHorde(){
     
     driveRelativeLeft(-5, 200);
     driveRelativeRight(-5, 200);
-    pvitChassis(-90, 200, 1);
+    pvitChassis(90, 200, 1);
     driveRelativeLeft(6, 200);
     driveRelativeRight(-6, 200);
     intakeLeft.set_value(0);
@@ -53,16 +53,16 @@ void autonRedFlagHorde(){
     driveRelativeRight(-5, 200);
 
     /*
-    pvitChassis(160, 200, 1.77778);
+    pvitChassis(-160, 200, 1.77778);
     driveRelativeLeft(35, 200);
     driveRelativeRight(35, 200);
     intake.move(127);
-    pvitChassis(-20, 200, 0.22222);
+    pvitChassis(20, 200, 0.22222);
     driveRelativeLeft(-35, 200);
     driveRelativeRight(-35, 200);
     intakeLift.set_value(1);
     intake.move(0);
-    pvitChassis(-140, 200, 1.55556);
+    pvitChassis(140, 200, 1.55556);
     intakeLift.set_value(0);
     delay(100);
     intake.move(-127);
