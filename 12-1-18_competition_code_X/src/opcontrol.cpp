ff3a49e8478b74e5a33cff516d87644266ef55b9
AaronG <aaronguo213@gmail.com>
2018-11-14 15:27:08 -0800

    edited driver control

--- 12-1-18_competition_code_X/src/opcontrol.cpp
+++ 12-1-18_competition_code_X/src/opcontrol.cpp
@@ -14,29 +14,24 @@ bool transmissionVar = 0, liftVar = 0, tipperVar = 0, atckL2, atckR2, atckY;
 		driveLeft((master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y))); //Left side of the base
 
 		if(master.get_digital(E_CONTROLLER_DIGITAL_R1)) //If R1 on the master controller is pressed
-
 			intake.move(127); //Then intake rolls balls and caps in
 		
 		else if(master.get_digital(E_CONTROLLER_DIGITAL_L1)) //If L1 on the master controller is pressed
-		
 			intake.move(-127); //Then intake flips caps
 
 		else
-
 			intake.move(0);
 
 
-		if(master.get_digital(E_CONTROLLER_DIGITAL_A)){
-
+		if(master.get_digital(E_CONTROLLER_DIGITAL_A))
 			aimFlag();
-			
-		}
 
-		if(master.get_digital(E_CONTROLLER_DIGITAL_UP)){ //If L2 and R2 on the master controller is pressed
-		
-			shooter.move_relative(1, 200); //Then shoot and reload
+		if(master.get_digital(E_CONTROLLER_DIGITAL_UP)) //If L2 and R2 on the master controller is pressed
+			shooter.move(127); //Then shoot and reload
 
-		}
+		else if(!master.get_digital(E_CONTROLLER_DIGITAL_UP))
+			shooter.move(0);
+
 
 		if(!master.get_digital(E_CONTROLLER_DIGITAL_L2))			
         	atckL2 = false;
