4c1f0922086eb5b1283cd2e218ad8e4dc3e3606c
Karl Velazquez <karlvelaz350@gmail.com>
2018-11-14 16:10:08 -0800

    even more stuff

--- 12-1-18_competition_code_P/src/opcontrol.cpp
+++ 12-1-18_competition_code_P/src/opcontrol.cpp
@@ -2,15 +2,6 @@
 
 //Runs during the 1 minute and 45 second period of driver control or immediately after initialize() if no competition control is connected
 void opcontrol() {
-<<<<<<< HEAD
-=======
-
-	bool transmissionState;
-	bool intakeState;
-
-	lcd::initialize();
-	//int setPoint = 3;
->>>>>>> ec5c0aa9137367dfa222277c742ef3966e243c4b
 	
 	while(true) {
 
@@ -35,48 +26,6 @@ void opcontrol() {
 
 		}
 
-<<<<<<< HEAD
-=======
-		if(master.get_digital(E_CONTROLLER_DIGITAL_R2)) {
-
-			transmissionState != transmissionState;
-			chassisTransmissionPiston.set_value(transmissionState);
-	
-		}
-
-		if(master.get_digital(E_CONTROLLER_DIGITAL_L2)) {
-
-			intakeState != intakeState;
-			intakePiston1.set_value(intakeState);
-			intakePiston2.set_value(intakeState);
-
-		}
-
-		if(master.get_digital(E_CONTROLLER_DIGITAL_X)) {
-
-			catapult.move_relative(1, 200);
-
-		}
-
-		else if(master.get_digital(E_CONTROLLER_DIGITAL_Y)) {
-
-			catapult.move_velocity(200);
-
-		}
-
-		else {
-
-			catapult.move_velocity(0);
-
-		}
-
-		if(master.get_digital(E_CONTROLLER_DIGITAL_UP)) {
-
-			aimAtFlag(1.0, 0.1);
-
-		}
-
->>>>>>> ec5c0aa9137367dfa222277c742ef3966e243c4b
 		delay(1);
 		
 		
