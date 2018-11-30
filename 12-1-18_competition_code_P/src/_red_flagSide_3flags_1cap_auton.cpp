bd9ba40914058652f40ba68438968aeeee55ed6c
Karl Velazquez <karlvelaz350@gmail.com>
2018-11-29 22:57:51 -0800

    made auton better

--- 12-1-18_competition_code_P/src/_red_flagSide_3flags_1cap_auton.cpp
+++ 12-1-18_competition_code_P/src/_red_flagSide_3flags_1cap_auton.cpp
@@ -3,19 +3,21 @@
 void red_flagSide_3flags_1caps_auton() {
 
     
-    driveStraight(2.32, 1500);
-    driveStraight(-.6, 500);
+    driveStraight(1.8, 1200);
     intake.move_velocity(500);
-    driveStraight(1.1, 500);
-    delay(100);
+    driveStraight(0.59, 800);
+    
+    
+    delay(375);
 
     driveStraight(-1.78, 1000);
-    loadBall(1500);
+    loadBall(1000);
     intake.move_velocity(0);
     delay(500);
-    pivotChassisBAD(-85, 200, 1000);
+    pivotChassisBAD(-83, 200, 1000);
     autonShoot();
     delay(150);
+    //pivotChassisBAD(15, 150, 800);
     driveStraight(-3, 1500);
 
 }
