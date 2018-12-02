1de035ee500e157ff71795840948cc2abd6e2f3c
Karl Velazquez <karlvelaz350@gmail.com>
2018-12-02 15:44:12 -0800

    made it so that math constants like pi can be used; we dont have to define them ourselves

--- 1-18-19_comp_code/include/main.h
+++ 1-18-19_comp_code/include/main.h
@@ -2,9 +2,8 @@
 #define _PROS_MAIN_H_ //DO NOT TOUCH
 
 #define PROS_USE_SIMPLE_NAMES //If defined, instead of using long names for enums like E_CONTROLLER_MASTER, use more convient names usch as CONTROLLER_MASTER
-
+#define _USE_MATH_DEFINES //If defined, math constants such as pi can be used
 #include "api.h" //Includes all of the functions, variables, etc. of the PROS 3 language
-
 using namespace pros; //Instead of typing pros::Motor() all the time, if you use this namepsace you can just do Motor()
 
 #ifdef __cplusplus //DO NOT TOUCH
