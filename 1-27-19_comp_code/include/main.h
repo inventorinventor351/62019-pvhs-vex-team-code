628d070fe59bc1fc500ca3e894d0165025334a02
Karl Velazquez <karlvelaz350@gmail.com>
2018-12-06 19:52:52 -0800

    created pragma.h and pragma.cpp

--- 1-27-19_comp_code/include/main.h
+++ 1-27-19_comp_code/include/main.h
@@ -12,7 +12,7 @@ using namespace pros; //Instead of typing pros::Motor() all the time, if you use
 #endif //DO NOT TOUCH
 
 //You should include more files right under here
-#include "1-18-19_comp_code\include\math.h"
+#include "misc.h"
 #include "pragma.h"
 
 //DO NOT TOUCH BELOW STUFF, THIS MAKES THE CODE RUN
---
+++ 1-27-19_comp_code/include/pragma.h
@@ -0,0 +1,19 @@
+#ifndef PRAGMA_H
+#define PRAGMA_H
+
+extern Motor leftBase1; //first motor on the left side of the base
+extern Motor leftBase2; //second motor on the left side of the base
+extern Motor rightBase1; //first motor on the right side of the base
+extern Motor rightBase2; //second motor on the right side of the base
+extern Motor intk; //motor that runs the intake
+extern Motor cplt; //motor that runs the catapult
+
+extern Vision intkVis; //vision sensor that helps the intake aim for balls
+extern Vision cpltVis; //vision sensor that helps the catapult aim for flags
+
+extern ADIDigitalOut transPstn; //piston that controls the transmission
+
+extern ADIEncoder distEnc; //encoder on a tracking wheel that is parallel to the base wheels
+extern ADIEncoder yawEnc; //encoder on a tracking wheel that is perpendicular to the base wheels
+
+#endif
