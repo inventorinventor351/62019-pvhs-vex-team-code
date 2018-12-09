175aefa35546d0e33f97cc607384559b7ffd4b3e
Karl Velazquez <karlvelaz350@gmail.com>
2018-12-09 15:54:32 -0800

    stuff

--- 1-27-19_comp_code/src/opcontrol.cpp
+++ 1-27-19_comp_code/src/opcontrol.cpp
@@ -2,10 +2,14 @@
 
 //Runs during the 1 minute and 45 second period of driver control or immediately after initialize() if no competition control is connected
 void opcontrol() {
+
+	std::uint_least32_t now = millis();
 	
 	while(true) {
 
-		
+
+
+		delay_until(&now, 1); //loop driver control 1000 times per second
 		
 	}
 
