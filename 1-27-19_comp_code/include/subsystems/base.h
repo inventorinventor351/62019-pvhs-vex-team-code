5da51d20604ed716a319768e9c9c516d3c4a9a4d
Karl Velazquez <karlvelaz350@gmail.com>
2018-12-16 14:52:14 -0800

    made base code

--- 1-27-19_comp_code/include/subsystems/base.h
+++ 1-27-19_comp_code/include/subsystems/base.h
@@ -4,10 +4,8 @@
 void runLeftBase(float voltPerc); //runs the left base at a percentage of max voltage from -100% to 100%
 void runRightBase(float voltPerc); //runs the right base at a percentage of max voltage from -100% to 100%
 
-extern int target;
+extern int leftTarget; //target voltage for slew rate
+extern int rightTarget; //target voltage for slew rate
 void baseSR(); //base's slew rate, runs evert 1 ms, controls accel of base
-void getBaseSpeed(); //returns angular speed of base every 2 ms, speed is in degrees per ms
-void basePosPID(); //runs base's position PID every 4 ms
-void baseVelPID(); //run base's velocity PID every 4 ms
 
 #endif
