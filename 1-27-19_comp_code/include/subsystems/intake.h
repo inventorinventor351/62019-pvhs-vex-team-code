4773db942aaf455ddd52c14d878ad91468962e4f
CameronG <cam.greene@gmail.com>
2018-12-16 13:45:34 -0800

    eef

--- 1-27-19_comp_code/include/subsystems/intake.h
+++ 1-27-19_comp_code/include/subsystems/intake.h
@@ -2,5 +2,6 @@
 #define INTAKE_H
 
 void runIntake(float voltPerc); //runs the intake at a percentage of max voltage from -100% to 100%
+void runIntakeTime(float voltPerc, int time); //runs the inyake at a percentage for a given amount of time
 
 #endif
--- 1-27-19_comp_code/src/subsystems/intake.cpp
+++ 1-27-19_comp_code/src/subsystems/intake.cpp
@@ -5,3 +5,12 @@ void runIntake(float voltPerc) {
     intk.move_voltage((voltPerc / 100) * 12000);
 
 }
+
+void runIntakeTime(float voltPerc, int time) {
+
+    intk.move_voltage((voltPerc / 100) * 12000)
+    delay(time);
+    intk.move_voltage(0);
+
+}
+
