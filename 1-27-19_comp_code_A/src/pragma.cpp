1f9ab223958858dac56f391292101d4cc88c888c
chris salas <topher1883@gmail.com>
2018-12-28 19:14:16 -0800

    kytd

--- 1-27-19_comp_code_A/src/pragma.cpp
+++ 1-27-19_comp_code_A/src/pragma.cpp
@@ -2,10 +2,10 @@
 
 Motor leftBase1 (1, MOTOR_GEARSET_18, 0); //normal motor plugged into port 1 and not reversed
 Motor leftBase2 (2, MOTOR_GEARSET_18, 0); //normal motor plugged into port 2 and not reversed
-Motor leftBase3 (3,MOTOR_GEARSET_18, 0); //normal motor plugged into port 3 and not reversed
+Motor leftBase3 (1, MOTOR_GEARSET_18, 1);
 Motor rightBase1 (3, MOTOR_GEARSET_18, 1); //normal motor plugged into port 3 and reversed
 Motor rightBase2 (4, MOTOR_GEARSET_18, 1); //normal motor plugged into port 4 and reversed
-Motor rightBase3 (5, MOTOR_GEARSET_18, 1); //normal motor plugged into port 5 and reversed
+Motor rightBase3 (5, MOTOR_GEARSET_18, 0);
 
 Motor intake (6, MOTOR_GEARSET_18, 0); //normal motor plugged into port 5 and not reversed
 
