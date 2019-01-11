68853b9d4553dc6e7f25679a4b15c28a774ca04f
AaronG <aaronguo213@gmail.com>
2019-01-10 16:35:47 -0800

    created autons folder

---
+++ 1-27-19_comp_code/src/autons/2cap3flag.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void 2cap3flag(){
+    
+    runIntake(80);
+    moveStraight();
+    moveStraight();
+    pvtBase();
+    flagAim();
+    moveStraight();
+    cpltShoot();
+    moveStraight();
+
+}
