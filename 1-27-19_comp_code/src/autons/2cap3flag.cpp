6aef9ec62fb2d78fe389578eaf7cc150fa337bdc
AaronG <aaronguo213@gmail.com>
2019-01-10 17:28:14 -0800

    h

--- 1-27-19_comp_code/src/autons/2cap3flag.cpp
+++ 1-27-19_comp_code/src/autons/2cap3flag.cpp
@@ -1,14 +1,21 @@
 #include "main.h"
 
-void 2cap3flag(){
+void 2cap3flag(){/*
     
     runIntake(80);
     moveStraight();
     moveStraight();
+    runIntake(0);
     pvtBase();
     flagAim();
     moveStraight();
     cpltShoot();
     moveStraight();
+    moveStraight();
+    pvtBase();
+    runIntake(-80);
+    moveStraight();
+    runIntake(0);
+    moveStraight();*/
 
 }
