bef14d77b2a5dcf376ab77f524caf6dd9f4858b0
Karl Velazquez <karlvelaz350@gmail.com>
2019-01-10 16:31:16 -0800

    yeah

--- 1-27-19_comp_code/src/autonomous.cpp
+++ 1-27-19_comp_code/src/autonomous.cpp
@@ -3,10 +3,6 @@
 //Runs during the 15 second autonomous period
 void autonomous() {
 
-    //Task baseSlewRate (baseSR, NULL, (TASK_PRIORITY_DEFAULT - 1), TASK_STACK_DEPTH_DEFAULT, "Base Slew Rate");
-
-    moveStraight(3600, 1, 2000);
-
-    //baseSlewRate.remove();
+    moveStraight(900, 1, 1000);
     
 }
