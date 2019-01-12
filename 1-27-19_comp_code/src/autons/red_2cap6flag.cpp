bc0aa8f45e6e596a98cc5e1f230a3ef2b9e15d13
AaronG <aaronguo213@gmail.com>
2019-01-11 19:23:44 -0800

    created flipCap, revised getBall, and created the 6 flag 2 cap auton red

--- 1-27-19_comp_code/src/autons/red_2cap6flag.cpp
+++ 1-27-19_comp_code/src/autons/red_2cap6flag.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void red_2cap3flag(){
+void red_2cap6flag(){
     
     /*runIntake(80);
     moveStraight();
@@ -12,13 +12,20 @@ void red_2cap3flag(){
     moveStraight();
     cpltShoot();
 
-    moveStraight();
+    getBall(2000);
     moveStraight();
     pvtBase();
 
     runIntake(-80);
     moveStraight();
     runIntake(0);
+
+    pvtBase(140, 2000);
+    flagAim();
+    moveStraight();
+    cpltShoot();
+    
+    moveStraight();
     moveStraight();*/
     
 }
