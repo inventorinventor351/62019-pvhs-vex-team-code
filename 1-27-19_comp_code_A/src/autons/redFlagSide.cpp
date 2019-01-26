55eaa32ab889031a01b9ecb981b6989744bb16b0
43engineering <43engineering@43PVITLAPTOP-05.pvpusdm.int>
2019-01-26 14:10:29 -0800

    hd

--- 1-27-19_comp_code_A/src/autons/redFlagSide.cpp
+++ 1-27-19_comp_code_A/src/autons/redFlagSide.cpp
@@ -3,9 +3,10 @@
 void redFlagSide()
 {
     runIntake(70);
-    drivePD(3);
-    delay(100);
     drivePD(2.9);
+    delay(300);
+    drivePD(-2.9);
+    delay(300);
     runIntake(0);
     pvitBase(90, 50);
     delay(100);
