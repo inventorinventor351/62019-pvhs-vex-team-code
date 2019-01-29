29567dc75d5831dbbbb79ff026a2abe2bd69404d
Karl Velazquez <karlvelaz350@gmail.com>
2019-01-28 17:18:47 -0800

    dsaf

--- 2-2-19_comp_code/include/autons.h
+++ 2-2-19_comp_code/include/autons.h
@@ -1,6 +1,9 @@
 #ifndef AUTONS_H
 #define AUTONS_H
 
-
+void blueCap();
+void redFlag();
+void redCap();
+void blueFlag();
 
 #endif
---
+++ 2-2-19_comp_code/src/misc.cpp
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int sgn(double x) { //inputs a double and outputs its sign (+ or -) as an int
+
+    return (x > 0) - (x < 0);
+
+}
+
+PID initPID(bool useP, bool useI, bool useD, double kP, double kI, double kD) { //gives a created PID struct its values
+
+    return { 0, 0, 0, 0, kP, kI, kD, useP, useI, useD };
+
+}
+
+double runPID(PID *pid) {
+
+    pid->integral += pid->error; //increment integral by current error
+    pid->derivative = pid->error - pid->prevError; //set derivative to the delta error
+    pid->prevError = pid->error; //set prevError to current error to be used for next iteration
+    return (pid->error * pid->kP * (double)pid->useP) + (pid->integral * pid->kI * (double)pid->useI) + (pid->derivative * pid->kD * (double)pid->useD); //calculate final value
+
+}
