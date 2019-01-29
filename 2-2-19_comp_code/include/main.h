c9dc1776a746a5cde9ed5bc04a9d1bb0b06cd97c
Karl Velazquez <karlvelaz350@gmail.com>
2019-01-28 16:31:12 -0800

    made 2-2-19_comp code

---
+++ 2-2-19_comp_code/include/main.h
@@ -0,0 +1,36 @@
+#ifndef _PROS_MAIN_H_
+#define _PROS_MAIN_H_
+
+#define PROS_USE_SIMPLE_NAMES
+
+#define PROS_USE_LITERALS
+
+#include "api.h"
+
+using namespace pros;
+
+#ifdef __cplusplus
+
+
+
+#endif
+
+#ifdef __cplusplus
+
+    extern "C" {
+
+#endif
+
+        void autonomous(void);
+        void initialize(void);
+        void disabled(void);
+        void competition_initialize(void);
+        void opcontrol(void);
+
+#ifdef __cplusplus
+
+    }
+
+#endif
+
+#endif
---
+++ 2-2-19_comp_code/src/initialize.cpp
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void on_center_button() {
+	
+
+	
+}
+
+void initialize() {
+
+
+
+}
+
+void competition_initialize() {
+
+
+
+}
+
+void disabled() {
+
+
+
+}
