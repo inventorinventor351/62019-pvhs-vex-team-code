2af71ab4eb254f0cc23e91e776dc43e241ad5712
Karl Velazquez <karlvelaz350@gmail.com>
2019-01-28 17:36:56 -0800

    yeh

--- 2-2-19_comp_code/src/subsystems/base.cpp
+++ 2-2-19_comp_code/src/subsystems/base.cpp
@@ -1,1 +1,54 @@
 #include "main.h"
+
+void runLeftBase(float voltPerc) {
+
+    leftBase1.move_voltage((voltPerc / 100) * 12000);
+    leftBase2.move_voltage((voltPerc / 100) * 12000);
+
+    if(PorX.get_value())
+        leftBase3.move_voltage((voltPerc / 100) * 12000);
+
+}
+
+void runRightBase(float voltPerc) {
+
+    rightBase1.move_voltage((voltPerc / 100) * 12000);
+    rightBase2.move_voltage((voltPerc / 100) * 12000);
+
+    if(PorX.get_value())
+        rightBase3.move_voltage((voltPerc / 100) * 12000);
+
+}
+
+void moveStraight(double setPoint, double direction, int time) {
+
+    double distVal, diffVal;
+ 
+    setPoint = abs(setPoint) * sgn(direction);
+
+    PID dist = initPID(1, 0, 1, 0.1, 0, 0.275);
+    PID diff = initPID(1, 0, 0, 0.8, 0, 0);
+
+    distEnc.reset();
+    gyro.reset();
+
+    for(int i = 0; i < time; i++) {
+
+        dist.error = setPoint - distEnc.get_value();
+        diff.error = (gyro.get_value() * 0.94);
+
+        distVal = runPID(&dist);
+        distVal = (abs(distVal) > 90) ? (90 * sgn(distVal)) : distVal;
+        diffVal = runPID(&diff);
+
+        runLeftBase(distVal - diffVal);
+        runRightBase(distVal + diffVal);
+
+        delay(1);
+
+    }
+
+    runLeftBase(0);
+    runRightBase(0);
+
+}
