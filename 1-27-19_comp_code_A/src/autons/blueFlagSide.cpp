36f351f582f2290b48debf37cc136576048ba292
43engineering <43engineering@43PVITLAPTOP-05.pvpusdm.int>
2019-02-10 13:27:10 -0800

    luyf

--- 1-27-19_comp_code_A/src/autons/blueFlagSide.cpp
+++ 1-27-19_comp_code_A/src/autons/blueFlagSide.cpp
@@ -6,25 +6,24 @@ void blueFlagSide()
    driveDist(3, 80);
    delay(100);
    runIntake(0);
-   driveDist(2.8, -70);
+   driveDist(1.5, -70);
    runLeftBase(60);
    runRightBase(-40);
-   delay(400);
+   delay(500);
    runLeftBase(0);
    runRightBase(0);
-   runIntake(-60);
-   driveDist(2.8, 70);
+   driveDist(4, 70);
    delay(200);
-   driveDist(2.8, -80);
+   driveDist(3, -80);
    runIntake(0);
    delay(200);
    runRightBase(-70);
-   delay(400);
+   delay(420);
    runRightBase(0);
    delay(300);
-   driveDist(4, 80);
+   runIntake(-60);
+   driveDist(3.5, 80);
+   delay(400);
+   runIntake(0);   
    
-   
-   
-
 }
