687da7def8c3bdd2a771884d790cdc996b011d5b
Karl Velazquez <karlvelaz350@gmail.com>
2019-02-10 15:26:05 -0800

    fdasjk

--- 2-16-19_comp_code/src/pragma.cpp
+++ 2-16-19_comp_code/src/pragma.cpp
@@ -3,12 +3,12 @@
 Controller master (CONTROLLER_MASTER); //master controller
 
 //V5 Components
-Motor leftBase1 (18, MOTOR_GEARSET_18, 0); //normal motor plugged into port 9 and not reversed
-Motor leftBase2 (17, MOTOR_GEARSET_18, 1); //normal motor plugged into port 7 and reversed
-Motor leftBase3 (19, MOTOR_GEARSET_18, 0); //normal motor plugged into port 8 and not reversed
-Motor rightBase1 (9, MOTOR_GEARSET_18, 1); //normal motor plugged into port 18 and reversed
-Motor rightBase2 (7, MOTOR_GEARSET_18, 0); //normal motor plugged into port 17 and not reversed
-Motor rightBase3 (8, MOTOR_GEARSET_18, 1); //normal motor plugged into port 19 and reversed
+Motor leftBase1 (18, MOTOR_GEARSET_18, 1); //normal motor plugged into port 9 and not reversed
+Motor leftBase2 (17, MOTOR_GEARSET_18, 0); //normal motor plugged into port 7 and reversed
+Motor leftBase3 (19, MOTOR_GEARSET_18, 1); //normal motor plugged into port 8 and not reversed
+Motor rightBase1 (9, MOTOR_GEARSET_18, 0); //normal motor plugged into port 18 and reversed
+Motor rightBase2 (7, MOTOR_GEARSET_18, 1); //normal motor plugged into port 17 and not reversed
+Motor rightBase3 (8, MOTOR_GEARSET_18, 0); //normal motor plugged into port 19 and reversed
 Motor intk (15, MOTOR_GEARSET_18, 1); //normal motor plugged into port 15 and not reversed
 Motor cplt (16, MOTOR_GEARSET_18, 0); //normal motor plugged into port 16 and not reversed
 
